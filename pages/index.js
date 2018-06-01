2c483cfe90033e32f6e4bc7e76d28e76e5ebe767
Jesse Weigel <jesseweigel@gmail.com>
2018-06-01 13:20:25 -0400

    init

---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import gql from 'graphql-tag'
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import App from '../components/App'
+
+class Home extends Component {
+  render() {
+    const PostsQuery = gql`
+      {
+        posts {
+          edges {
+            node {
+              id
+              slug
+              title
+              date
+              content
+            }
+          }
+        }
+      }
+    `
+    return (
+      <App>
+        <Query query={PostsQuery}>
+          {result => {
+            if (result.loading) {
+              return <h1>Loading</h1>
+            }
+            if (result.error) return <h3>{result.error}</h3>
+
+            const { data } = result
+            const posts = data.posts.edges
+
+            return (
+              <ul className="no-bullet">
+                <li>
+                  <h2>Posts</h2>
+                </li>
+                {posts.map(post => (
+                  <li key={post.node.slug}>
+                    <h3
+                      dangerouslySetInnerHTML={{
+                        __html: post.node.title,
+                      }}
+                    />
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: post.node.content,
+                      }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            )
+          }}
+        </Query>
+      </App>
+    )
+  }
+}
+
+export default Home
